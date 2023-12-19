f68c18c885690d80f6b17b8a3a6a2c8133684753
mradbourne <1254508+mradbourne@users.noreply.github.com>
2023-12-19 11:02:04 +0000

    Add test data generator

--- test/testdata_gen.py
+++ test/testdata_gen.py
@@ -1,28 +1,73 @@
-import glob, os, json
-import yaml
+import glob
+import os
+import json
+from pathlib import Path
+from yaml import safe_load, scanner
+from jsonschema import Draft202012Validator, ValidationError
 
-# with open('testgen/config.yml', 'r') as yaml_file:
-#     configuration = yaml.safe_load(yaml_file)
-
-# with open('testgen/config.json', 'w') as json_file:
-#     json.dump(configuration, json_file)
+JSON_INDENT = 2
 
 
-# output = json.dumps(json.load(open('testgen/config.json')), indent=2)
-# print(output)
 def main():
     test_dir = os.path.normpath(
         os.path.join(os.path.dirname(__file__), "testgen", "src")
     )
     os.chdir(test_dir)
-    for test_file in glob.glob("./**/*.test.yml", recursive=True):
-        build_json(test_file)
+    test_files = glob.glob("./**/*.ddt.yml", recursive=True)
+
+    if len(test_files) == 0:
+        print("No tests found.")
+
+    schema_path = os.path.join(os.path.dirname(__file__), "testgen", "ddt.schema.json")
+    validator = Draft202012Validator(json.load(open(schema_path)))
+
+    for src_path in test_files:
+        with open(src_path, "r") as file:
+            try:
+                tests = safe_load(file)
+                validator.validate(tests)
+                build_test_json(src_path, tests)
+                build_verify_json(src_path, tests)
+            except scanner.ScannerError as err:
+                print_error(f"Could not load {src_path}", err)
+            except ValidationError as err:
+                print_error(f"Could not validate {src_path}", err)
+            except Exception as err:
+                print_error(f"Could not build {src_path}", err)
 
 
-def build_json(file):
-    with open(file, "r") as yaml_file:
-        test_dict = yaml.safe_load(yaml_file)
-        print(type(configuration))
+def print_error(user_message, err):
+    err_str = f"{type(err).__name__}: {err.args}"
+    print(f"{user_message}:\n", err_str, "\n")
+
+
+def build_test_json(src_path, tests):
+    destination = get_destination_path(src_path, ".test.json")
+    build_json(
+        tests, destination, ["label", "description", "locale", "pattern", "inputs"]
+    )
+
+
+def build_verify_json(src_path, tests):
+    destination = get_destination_path(src_path, ".verify.json")
+    build_json(tests, destination, ["label", "verify"])
+
+
+def get_destination_path(src_path, file_extension):
+    rel_dir = os.path.dirname(src_path)
+    basename = file_extension.join(os.path.basename(src_path).rsplit(".ddt.yml", 1))
+    return Path(os.path.dirname(__file__), "DDT_DATA", rel_dir, basename)
+
+
+def build_json(tests, destination_path, fields):
+    verifications = [
+        {field: test.get(field) for field in fields if test.get(field) != None}
+        for test in tests["tests"]
+    ]
+    output = {"scenario": tests["scenario"], "verifications": verifications}
+    destination_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(destination_path, "w") as json_file:
+        json.dump(output, json_file, indent=JSON_INDENT)
 
 
 if __name__ == "__main__":
