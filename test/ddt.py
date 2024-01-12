333699fe43589ec2285a555e11e19ac946a93384
mradbourne <1254508+mradbourne@users.noreply.github.com>
2024-01-12 15:58:29 +0000

    Consolidate run and build scripts

--- test/ddt.py
+++ test/ddt.py
@@ -1,14 +1,42 @@
+import sys
+
+sys.dont_write_bytecode = True
+
+import argparse
 import glob
 import os
 import json
 from pathlib import Path
 from yaml import safe_load, scanner
 from jsonschema import Draft202012Validator, ValidationError
+from executors import executors
 
 JSON_INDENT = 2
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest="command")
+    subparser.add_parser("build")
+    run_parser = subparser.add_parser("run")
+
+    run_parser.add_argument("executor", choices=executors.keys())
+    run_parser.add_argument("action", choices=["init", "test"])
+    run_parser.add_argument("-b", "--build", action=argparse.BooleanOptionalAction)
+
+    args = parser.parse_args()
+
+    if args.command == "run":
+        if args.build:
+            build()
+        print(f"\nSending {args.action} command to {args.executor} executor...")
+        getattr(executors[args.executor], args.action)()
+    elif args.command == "build":
+        build()
+
+
+def build():
+    print("\nBuilding JSON from YAML...")
     test_dir = os.path.normpath(
         os.path.join(os.path.dirname(__file__), "testgen", "src")
     )
@@ -78,6 +106,8 @@ def build_json(tests, destination_path, scenario_fields, test_fields):
     destination_path.parent.mkdir(parents=True, exist_ok=True)
     with open(destination_path, "w") as json_file:
         json.dump(output, json_file, indent=JSON_INDENT)
+
+    print(f"Written {destination_path}")
 
 
 if __name__ == "__main__":
